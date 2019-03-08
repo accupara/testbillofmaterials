2314507bdcc0540afa1fd3e564c9f0789614cff5
Imran Khan <imran@accupara.com>
2019-03-08 15:07:05 +0530

    Initial commit

---
+++ src/input.h
@@ -0,0 +1,7 @@
+#ifdef __INPUT_H__
+
+#define __INPUT_H__
+
+void print_hello_world();
+
+#endif //__INPUT_H__
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include <input.h>
+
+int main(void) {
+	print_hello_world();
+	return 0;
+}
